f177d78a501bb51de37863838098ff83099404d7
Chitra Dhana Sri <86092445+Chitra1711@users.noreply.github.com>
2023-10-30 12:06:12 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,46 @@
+# Function to perform addition
+def add(x, y):
+    return x + y
+
+# Function to perform subtraction
+def subtract(x, y):
+    return x - y
+
+# Function to perform multiplication
+def multiply(x, y):
+    return x * y
+
+# Function to perform division
+def divide(x, y):
+    if y == 0:
+        return "Division by zero is not allowed"
+    return x / y
+
+# Function to perform the calculator operation
+def calculator():
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+
+    choice = input("Enter choice (1/2/3/4): ")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: ")
+
+    if choice == '1':
+        result = add(num1, num2)
+    elif choice == '2':
+        result = subtract(num1, num2)
+    elif choice == '3':
+        result = multiply(num1, num2)
+    elif choice == '4':
+        result = divide(num1, num2)
+    else:
+        result = "Invalid input"
+
+    print("Result: ", result)
+
+# Run the calculator
+calculator()
